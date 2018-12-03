ce860d860bfead0a3d91bcfdb2bc249217257d8a
Richard Denton <isdampe@gmail.com>
2018-12-03 19:25:20 +1100

    EXAMPLE: ltrim and rtrim.

--- examples/example.c
+++ examples/example.c
@@ -33,12 +33,24 @@ int main(int argc, char **argv)
 	strip->strip(strip2);
 	printf("'%s'\n", strip2->c_str(strip2));
 
+	string lstrip = string_create("   It's Richard.");
+	printf("'%s'\n", lstrip->c_str(lstrip));
+	lstrip->ltrim(lstrip);
+	printf("'%s'\n", lstrip->c_str(lstrip));
+
+	string rstrip = string_create("It's Richard.       ");
+	printf("'%s'\n", rstrip->c_str(rstrip));
+	lstrip->rtrim(rstrip);
+	printf("'%s'\n", rstrip->c_str(rstrip));
+
 
 	string_destroy(welcome);
 	string_destroy(name);
 	string_destroy(name_copy);
 	string_destroy(test1);
 	string_destroy(test2);
+	string_destroy(lstrip);
+	string_destroy(rstrip);
 
 	return 0;
 }
